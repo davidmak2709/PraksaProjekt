6bf08fac5433a6f216d95d96de510401c7d34269
David Makovac <davidmakovac95@gmail.com>
2018-07-05 15:35:51 +0200

    Reset password na login screenu

--- MobileBudget/app/screens/Login/LoginScreen.js
+++ MobileBudget/app/screens/Login/LoginScreen.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
-import {AsyncStorage,AppRegistry,View, StyleSheet,Text} from 'react-native';
-import { Button, FormInput, FormValidationMessage  } from 'react-native-elements';
+import {AsyncStorage,AppRegistry,View, StyleSheet,Text,Dimensions,KeyboardAvoidingView } from 'react-native';
+import { Button, FormInput, FormValidationMessage, } from 'react-native-elements';
 
 
 
-
+const {height, width} = Dimensions.get('window');
 export default class LoginScreen extends Component {
     static navigationOptions = {
       header: null,
@@ -68,32 +68,73 @@ export default class LoginScreen extends Component {
       }
     }
 
+    resetPassword(){
+      const email = this.state.email;
+      let emailError = false;
+      let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      if(email == "" || !reg.test(email)){
+        emailError = true;
+        this.setState({borderColorEmail : "red"});
+        this.emailRef.shake();
+      } else{
+        this.setState({borderColorEmail : "green"});
+        emailError = false;
+      }
+
+      console.log(emailError);
+      if(emailError === false){
+        fetch('http://46.101.226.120:8000/api/rest-auth/password/reset/', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              email: email,
+            }),
+          }).then((response) => {
+              console.log(response);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+      }
+
+      this.setState({emailError : emailError});
+
+    }
 
     render(){
       const { navigate } = this.props.navigation;
       return(
-        <View style = {styles.container}>
+        <KeyboardAvoidingView style = {styles.container} enable>
           <FormInput placeholder = "Insert your Email" containerStyle = {[styles.input ,{borderColor : this.state.borderColorEmail}]}
-              inputStyle = {{width : 250 }} ref={emailRef => this.emailRef = emailRef} underlineColorAndroid="transparent"
-                selectionColor = "orangered" autoCapitalize = "none" onSubmitEditing = { () => this.passwordRef.focus()}
+              inputStyle = {{width : width - 100 }} ref={emailRef => this.emailRef = emailRef} underlineColorAndroid="transparent"
+               autoCapitalize = "none" onSubmitEditing = { () => this.passwordRef.focus()}
                   blurOnSubmit={false}  onChangeText = {(email) => this.setState({email})} keyboardType = "email-address"
                     returnKeyType = "next" />
 
             {this.state.emailError  ? <FormValidationMessage>Error email</FormValidationMessage> : null }
 
-        <FormInput placeholder = "Insert your Password" containerStyle = {[styles.input ,{borderColor : this.state.borderColorPwd}]}
-            inputStyle = {{width : 250}}  secureTextEntry = {true} underlineColorAndroid="transparent" selectionColor = "orangered"
-             autoCapitalize = "none" ref={passwordRef => this.passwordRef = passwordRef} onChangeText = {(password) => this.setState({password})}
-              returnKeyType = "done"/>
+          <FormInput placeholder = "Insert your Password" containerStyle = {[styles.input ,{borderColor : this.state.borderColorPwd}]}
+              inputStyle = {{width : width - 100}}  secureTextEntry = {true} underlineColorAndroid="transparent"
+               autoCapitalize = "none" ref={passwordRef => this.passwordRef = passwordRef} onChangeText = {(password) => this.setState({password})}
+                returnKeyType = "done"/>
 
-            {this.state.passwordError ? <FormValidationMessage>Error password</FormValidationMessage> : null }
+              {this.state.passwordError ? <FormValidationMessage>Error password</FormValidationMessage> : null }
 
 
-        <View style = {styles.buttonsContainer}>
-            <Button raised title = "Login"   backgroundColor="green"  buttonStyle = {styles.button} onPress = {this.loginUser.bind(this)}/>
-            <Button raised title = "Sign Up" backgroundColor="darkred" buttonStyle = {styles.button} onPress = {() => navigate('Signup')}/>
+          <View style = {styles.buttonsContainer}>
+              <Button raised title = "Login"  icon = {{ name : "person" , size : 20 }}  backgroundColor="green"
+                  buttonStyle = {styles.mainButton} onPress = {this.loginUser.bind(this)}/>
+
+              <Button raised title = "Sign Up" icon = {{ name : "person-add" , size : 20 }} backgroundColor="darkred"
+                  buttonStyle = {styles.mainButton} onPress = {() => navigate('Signup')}/>
           </View>
-        </View>
+              <Button raised icon = {{ name : "https" , size : 20 }} title = "Forgot password" backgroundColor="lightblue"
+                  buttonStyle = {styles.passwordButton} onPress = {this.resetPassword.bind(this)}/>
+          </KeyboardAvoidingView>
+
       );
   }
 
@@ -110,17 +151,23 @@ const styles = StyleSheet.create({
     },
     buttonsContainer : {
       marginTop : 50,
+      marginBottom: 30,
       flexDirection : "row"
     },
-    button : {
-      width : 100,
+    mainButton : {
+      width : width * 0.35,
       borderColor: "transparent",
-      borderWidth: 0
+      borderWidth: 0,
+    },
+    passwordButton : {
+      width : width * 0.5,
+      borderColor: "transparent",
+      borderWidth: 0,
     },
     input : {
       margin : 10,
       alignItems : "center",
-      width :300,
+      width : width - 50,
       borderRadius : 50,
       backgroundColor : "ghostwhite",
       borderWidth : 2,
