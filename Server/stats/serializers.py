a2eec77e05c8d9b6afe32d82bd0dcbba653af32f
lratkajec <luka.ratkajec@gmail.com>
2018-08-09 13:21:22 +0200

    Caching statistike

--- Server/stats/serializers.py
+++ Server/stats/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class CategoryStatsSerializer(serializers.Serializer):
     category = serializers.CharField(max_length=20)
     percentage = serializers.FloatField()
+    type = serializers.CharField(max_length=5)
--- Server/stats/views.py
+++ Server/stats/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
@@ -9,6 +12,8 @@ from . import serializers
 class CategoryStatsView(APIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+    @method_decorator(cache_page(60*60*24))
+    @method_decorator(vary_on_cookie)
     def get(self, request, format=None):
         current_user = self.request.user
         if request.GET.get('type', 'out')=='out':
@@ -31,6 +36,6 @@ class CategoryStatsView(APIView):
             percentage = (cumulative_category/cumulative)*100
         else:
             percentage = 0
-        data = [{'category': request.GET.get('category', ''), 'percentage': percentage}]
+        data = [{'category': request.GET.get('category', ''), 'percentage': percentage, 'type': request.GET.get('type', 'out')}]
         results = serializers.CategoryStatsSerializer(data, many=True).data
         return Response(results)
